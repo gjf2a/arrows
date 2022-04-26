3a032c2cf7e22bf53fe29a5de2685c195c57b29d
gjf2a <ferrer@hendrix.edu>
2022-04-26 15:26:53 -0500

    Exit is a little cleaner.

--- lib.py
+++ lib.py
@@ -1,3 +1,5 @@
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+
 class Action:
     def __init__(self, left, right, speed):
         self.left = left
@@ -33,6 +35,8 @@ def buttonLoop(ev3, action_map):
     while True:
         pressed = ev3.buttons.pressed()
         if len(pressed) > 0:
+            if pressed[0] == Button.LEFT_UP:
+                return
             action_map[pressed[0]].act()
 
 TEXT_HEIGHT = 16
