9da6b9c41a674ce0a06853b86a29eab1de2ba152
gjf2a <ferrer@hendrix.edu>
2022-04-26 15:42:20 -0500

    Cleaned up UI a little.

--- driver.py
+++ driver.py
@@ -9,8 +9,9 @@ def drive(ev3, left, right):
         Button.DOWN:   lib.Backward(left, right, 360),
         Button.CENTER: lib.Stop(left, right, 360)
     }
+    
+    lib.buttonLoop(ev3, action_map, instructions)
 
-    ev3.screen.clear()
+def instructions(ev3):
     for i, s in enumerate(["UP -> Forward", 'LEFT -> Turn left', 'RIGHT -> Turn right', 'DOWN -> Backward', 'CENTER -> Stop', 'UP_LEFT -> Quit']):
-        ev3.screen.draw_text(0, i * lib.TEXT_HEIGHT, s)
-    lib.buttonLoop(ev3, action_map)
+        ev3.screen.draw_text(0, (i + 1) * lib.TEXT_HEIGHT, s)
--- lib.py
+++ lib.py
@@ -31,12 +31,16 @@ class Stop(Action):
     def speeds(self):
         return 0, 0
 
-def buttonLoop(ev3, action_map):
+def buttonLoop(ev3, action_map, instructions):
+    instructions(ev3)
     while True:
         pressed = ev3.buttons.pressed()
         if len(pressed) > 0:
             if pressed[0] == Button.LEFT_UP:
                 return
+            ev3.screen.clear()
+            instructions(ev3)
+            ev3.screen.draw_text(0, 0, str(pressed[0]))
             action_map[pressed[0]].act()
 
 TEXT_HEIGHT = 16
